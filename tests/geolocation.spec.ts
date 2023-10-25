1da9612216c463f5714185db8ec0a3cce9904a38
moidor <moidor44@hotmail.com>
2023-10-25 22:46:24 +0200

    Geolocation: test zip file tracing added

--- tests/geolocation.spec.ts
+++ tests/geolocation.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, Locator, Browser, defineConfig } from '@playwright/test';
+import { test, expect, Locator, Browser } from '@playwright/test';
 
 
 test.describe('geolocation', () => {
@@ -11,12 +11,23 @@ test.describe('geolocation', () => {
     offline: false
   });
 
-  test('geolocation Bing Maps', async ({ page }) => {
-    test.slow();
-    await page.goto('https://www.bing.com/maps');
-    await page.getByRole('button', { name: 'Refuser' }).click();
-    await page.getByRole('button', { name: 'Me localiser' }).click();
-    await page.getByRole('button', { name: 'Fermer' }).click();
+  test('geolocation Bing Maps', async ({ page, browser }) => {
+    // test.slow();
+    const isBrowserConnected = await browser.isConnected();
+    console.log(isBrowserConnected);
+    if (isBrowserConnected) {
+      // await browser.startTracing(page, { path: 'trace.json' });
+      const context = await browser.newContext();
+      await context.tracing.start({ screenshots: true, snapshots: true });
+      await page.goto('https://www.bing.com/maps');
+      await page.getByRole('button', { name: 'Refuser' }).click();
+      await page.getByRole('button', { name: 'Me localiser' }).click();
+      await page.getByRole('button', { name: 'Fermer' }).click();
+      await context.tracing.stop({ path: 'docs/traces/trace_bing_maps.zip' });
+    } else {
+      console.log("A problem occurred");
+    }
+    
   });
 
   test('geolocation Google Maps', async ({ page }) => {
