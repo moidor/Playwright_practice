42acaa10a3d016d28906e730b6f624228ab6c3ea
moidor <moidor44@hotmail.com>
2023-12-02 10:21:26 +0100

    filechoose finally working even if the download page leads to a dead page...

--- tests/filechooser.spec.ts
+++ tests/filechooser.spec.ts
@@ -1,25 +1,30 @@
 import { test, expect, Page, Locator, Browser, devices, chromium } from '@playwright/test';
 import path from 'path';
 
-test('filechooser',async ({ page, browser }) => {
-  test.slow();
-  const context = await browser.newContext();
-  await context.grantPermissions(['geolocation']);
+test.describe('filechoose test.describe()', () => {
+  test.use({
+    locale: '{ fr-FR }'
+  });
 
-  await page.goto('https://google.fr/', { waitUntil: "load" });
-  await page.getByRole('button', { name: 'Tout refuser' }).click();
-  await page.getByRole('button', { name: 'Rechercher par image' }).click();
-  // Start waiting for file chooser before clicking. Note no await.
-  const fileChooserPromise = page.waitForEvent('filechooser');
+  test('filechooser',async ({ page, browser }) => {
+    test.slow();
+    const context = await browser.newContext();
+    await context.grantPermissions(['geolocation']);
   
-  await page.getByText('importez un fichier').click();
-  const fileChooser = await fileChooserPromise;
-  // console.log(fileChooser.isMultiple());
-  // console.log(fileChooser.element()); 
-  await fileChooser.setFiles(path.join('misc\\chat.jpg'));
-  await page.getByRole('button', { name: 'Tout refuser' }).click();
+    await page.goto('https://google.fr/', { waitUntil: "load" });
+    await page.getByRole('button', { name: 'Tout refuser' }).click();
+    await page.getByRole('button', { name: 'Rechercher par image' }).click();
+    
+    const fileChooserPromise = page.waitForEvent('filechooser');
+    await page.getByRole('button', { name: 'upload a file' }).click();
+    const fileChooser = await fileChooserPromise;
+    await fileChooser.setFiles(path.join('D:\\Sauvegarde travaux Cham\\Playwright\\misc\\', 'chat.jpg'));
 
-  context.clearPermissions();
-  // await browser.close();
-});
-
+    await page.getByRole('button', { name: 'Tout refuser' }).click();
+    // The Google's website doesn't still work... As a result, we can import later with the next buttons.
+    // await page.getByRole('button', { name: 'Importer' }).click();
+    // await page.getByLabel('Ordinateur').click();    
+    context.clearPermissions();
+    // await browser.close();
+  });
+})
